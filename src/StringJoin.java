16dd96287c2e4a9176ab148662fe0aab45f81a2a
Satwik <thisisanemailadressbro99@gmail.com>
2026-04-13 20:26:50 +0530

    UC7: Add String.join() to concatenate multiple names

---
+++ src/StringJoin.java
@@ -0,0 +1,10 @@
+public class StringJoin {
+    public static void main(String[] args) {
+        if (args.length == 0) {
+            System.out.println("Hello, World!");
+        } else {
+            String result = String.join(", ", args);
+            System.out.println("Hello, " + result + "!");
+        }
+    }
+}
